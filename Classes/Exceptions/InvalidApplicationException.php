bff719eb1a6c1c659c2ea03400ac5fd654986dd7
Florian Wessels <f.wessels@bitmotion.de>
2018-08-21 17:37:41 +0200

    [TASK] Add Exception classes

---
+++ Classes/Exceptions/InvalidApplicationException.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace Bitmotion\Auth0\Exception;
+
+/***
+ *
+ * This file is part of the "Auth0 for TYPO3" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ *  (c) 2018 Florian Wessels <f.wessels@bitmotion.de>, Bitmotion GmbH
+ *
+ ***/
+
+
+/**
+ * Class InvalidConfigurationValueException
+ *
+ * @package Bitmotion\Auth0\Exception
+ */
+class InvalidApplicationException extends \Exception
+{
+
+}
