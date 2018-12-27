b17a338d73fc1e43dbef1763c03dbcd6abfa9ede
Florian Wessels <f.wessels@bitmotion.de>
2018-12-27 13:43:44 +0100

    [BUGFIX] Use proper variable

--- Classes/Utility/UpdateUtility.php
+++ Classes/Utility/UpdateUtility.php
@@ -149,7 +149,7 @@ class UpdateUtility implements SingletonInterface
                 $queryBuilder->execute();
             }
         } catch (\Exception $exception) {
-            $this->logger->error($e->getCode() . ': ' . $e->getMessage());
+            $this->logger->error($exception->getCode() . ': ' . $exception->getMessage());
         }
     }
 
