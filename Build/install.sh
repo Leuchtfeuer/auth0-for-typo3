57f518a8b1f6239b0545f5f2bcbe63bead625dab
Florian Wessels <f.wessels@bitmotion.de>
2019-02-11 16:52:09 +0100

    [TASK] Use proper version of typo3/testing-framework for dev-master

--- Build/install.sh
+++ Build/install.sh
@@ -4,7 +4,11 @@ if [[ "${TYPO3_VERSION}" = *"dev"*  ]]; then
     composer config minimum-stability dev
 fi
 
-composer req friendsofphp/php-cs-fixer:"${PHP_CS_FIXER_VERSION}" typo3/cms-core:"${TYPO3_VERSION}" typo3/cms-backend:"${TYPO3_VERSION}" typo3/cms-extbase:"${TYPO3_VERSION}" typo3/cms-extensionmanager:"${TYPO3_VERSION}" typo3/cms-fluid:"${TYPO3_VERSION}" typo3/cms-frontend:"${TYPO3_VERSION}"
+if [[ "${TYPO3_VERSION}" = "dev-master" ]]; then
+    composer req typo3/testing-framework:^5.0 friendsofphp/php-cs-fixer:"${PHP_CS_FIXER_VERSION}" typo3/cms-core:"${TYPO3_VERSION}" typo3/cms-backend:"${TYPO3_VERSION}" typo3/cms-extbase:"${TYPO3_VERSION}" typo3/cms-extensionmanager:"${TYPO3_VERSION}" typo3/cms-fluid:"${TYPO3_VERSION}" typo3/cms-frontend:"${TYPO3_VERSION}"
+else
+    composer req friendsofphp/php-cs-fixer:"${PHP_CS_FIXER_VERSION}" typo3/cms-core:"${TYPO3_VERSION}" typo3/cms-backend:"${TYPO3_VERSION}" typo3/cms-extbase:"${TYPO3_VERSION}" typo3/cms-extensionmanager:"${TYPO3_VERSION}" typo3/cms-fluid:"${TYPO3_VERSION}" typo3/cms-frontend:"${TYPO3_VERSION}"
+fi
 
 
 # --------------------------------------------------------------------------- #
