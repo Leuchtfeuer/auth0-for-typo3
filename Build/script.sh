7c599511d9d9c2bfbd8848709b109d1d83a9a26a
Florian Wessels <f.wessels@bitmotion.de>
2019-02-11 16:35:14 +0100

    [TASK] Do not rm file

--- Build/script.sh
+++ Build/script.sh
@@ -31,6 +31,3 @@ fi
 
 echo "Running functional API tests"
 .Build/bin/phpunit --colors --configuration Tests/Build/FunctionalTests.xml Tests/Functional --bootstrap .Build/vendor/typo3/testing-framework/Resources/Core/Build/FunctionalTestsBootstrap.php --coverage-clover=coverage.integration.clover
-
-
-rm $APPLICATION_FILE
