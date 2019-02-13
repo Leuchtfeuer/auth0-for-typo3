94798313e6f5aa4a40fdf186f73b6716770040fd
Florian Wessels <f.wessels@bitmotion.de>
2019-02-13 17:25:32 +0100

    [TASK] Add user block object

---
+++ Classes/Domain/Model/Auth0/UserBlock.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+namespace Bitmotion\Auth0\Domain\Model\Auth0;
+
+class UserBlock
+{
+    /**
+     * Array of identifier + ip pairs
+     *
+     * @var array
+     */
+    protected $blockedFor;
+
+    /**
+     * @return array
+     */
+    public function getBlockedFor()
+    {
+        return $this->blockedFor;
+    }
+
+    public function setBlockedFor(array $blockedFor)
+    {
+        $this->blockedFor = $blockedFor;
+    }
+}
