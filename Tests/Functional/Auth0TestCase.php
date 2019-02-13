1649324e9ba4e7c9b2a5d5dde4056ba5bcd7ce13
Florian Wessels <f.wessels@bitmotion.de>
2019-02-13 19:00:18 +0100

    [BUGFIX] Add missing braces

--- Tests/Functional/Auth0TestCase.php
+++ Tests/Functional/Auth0TestCase.php
@@ -109,8 +109,8 @@ abstract class Auth0TestCase extends FunctionalTestCase
         $file = __DIR__ . '/Fixtures/auth0_user.yml';
         /** @var User $user */
         $user = $serializer->deserialize(file_get_contents($file), User::class, 'yml');
-        $user->setEmail(sprintf($user->getEmail(), uniqid());
-        $user->setNickname(sprintf($user->getNickname(), uniqid());
+        $user->setEmail(sprintf($user->getEmail(), uniqid()));
+        $user->setNickname(sprintf($user->getNickname(), uniqid()));
 
         $data = $serializer->normalize($user, 'array', [
             ObjectNormalizer::ATTRIBUTES => UserApi::ALLOWED_ATTRIBUTES_CREATE,
