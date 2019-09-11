c0f6785bcab66d5d83df057e568efa0e0c11d581
Florian Wessels <f.wessels@bitmotion.de>
2019-09-11 09:59:59 +0200

    [TASK] Do not restore Auth0 session

--- Classes/Middleware/Auth0Middleware.php
+++ Classes/Middleware/Auth0Middleware.php
@@ -3,14 +3,12 @@ declare(strict_types=1);
 namespace Bitmotion\Auth0\Middleware;
 
 use Auth0\SDK\Store\SessionStore;
-use Bitmotion\Auth0\Service\SessionService;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerAwareTrait;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class Auth0Middleware implements MiddlewareInterface, LoggerAwareInterface
 {
@@ -26,22 +24,8 @@ class Auth0Middleware implements MiddlewareInterface, LoggerAwareInterface
         $userInfo = $sessionStore->get('user');
 
         if ($userInfo === null && is_array($feUserAuthentication->user) && $feUserAuthentication->user['auth0_user_id'] !== '') {
-            $applicationUid = (int)$feUserAuthentication->user['auth0_last_application'];
-            $sessionService = GeneralUtility::makeInstance(SessionService::class);
-
-            try {
-                $sessionService->createAuth0Session($feUserAuthentication, $sessionStore, $applicationUid);
-            } catch (\Exception $exception) {
-                $this->logger->warning(
-                    sprintf(
-                        'Could not update Auth0 Session. - %s: %s',
-                        $exception->getCode(),
-                        $exception->getMessage()
-                    )
-                );
-                $this->logger->notice('Logoff user.');
-                $feUserAuthentication->logoff();
-            }
+            $this->logger->notice('Logoff user.');
+            $feUserAuthentication->logoff();
         }
 
         return $handler->handle($request);
