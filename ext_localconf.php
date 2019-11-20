678cd75ce3fc59e5b381170b506080c5c7e4badb
Florian Wessels <f.wessels@bitmotion.de>
2019-11-20 16:06:33 +0100

    [TASK] Refactor and clean up registration files

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,92 +1,76 @@
 <?php
-defined('TYPO3_MODE') || die();
+defined('TYPO3_MODE') || die('Access denied.');
 
-if (!defined('TYPO3_COMPOSER_MODE') || !TYPO3_COMPOSER_MODE) {
-    require \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('auth0') . 'Libraries/vendor/autoload.php';
-}
-
-if (version_compare(TYPO3_version, '10.0.0', '>=')) {
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-        'Auth0',
-        'LoginForm',
-        [\Bitmotion\Auth0\Controller\LoginController::class => 'form, login, logout'],
-        [\Bitmotion\Auth0\Controller\LoginController::class => 'form, login, logout']
-    );
-} else {
-    // TODO: Remove this when dropping TYPO3 9 LTS support.
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-        'Bitmotion.Auth0',
-        'LoginForm',
-        ['Login' => 'form, login, logout'],
-        ['Login' => 'form, login, logout']
-    );
-}
-
-
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['list_type_Info']['auth0_loginform']['auth0'] =
-    \Bitmotion\Auth0\Hooks\PageLayoutViewHook::class . '->getSummary';
-
-
-
-$configuration = new \Bitmotion\Auth0\Domain\Transfer\EmAuth0Configuration();
-if ($configuration->getEnableBackendLogin() === true) {
-    $subtypes = 'authUserFE,getUserFE,getUserBE,authUserBE';
-    if (TYPO3_MODE === 'BE') {
-        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['backend']['loginProviders'][\Bitmotion\Auth0\LoginProvider\Auth0Provider::LOGIN_PROVIDER] = [
-            'provider' => \Bitmotion\Auth0\LoginProvider\Auth0Provider::class,
-            'sorting' => 25,
-            'icon-class' => 'fa-sign-in',
-            'label' => 'LLL:EXT:auth0/Resources/Private/Language/locallang.xlf:backendLogin.switch.label'
-        ];
-    }
-} else {
-    $subtypes = 'authUserFE,getUserFE';
-}
-
-$highestPriority = 0;
-
-if (is_array($GLOBALS['T3_SERVICES']['auth'])) {
-    foreach ($GLOBALS['T3_SERVICES']['auth'] as $service) {
-        if ($service['priority'] > $highestPriority) {
-            $highestPriority = $service['priority'];
+call_user_func(
+    function ($extensionKey) {
+        // Load libraries when TYPO3 is not in composer mode
+        if (!defined('TYPO3_COMPOSER_MODE') || !TYPO3_COMPOSER_MODE) {
+            require \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($extensionKey) . 'Libraries/vendor/autoload.php';
         }
-    }
-}
 
-$overrulingPriority = $highestPriority + 10;
+        // Define some variables depending on TYPO3 Version
+        // TODO: Remove this when dropping TYPO3 9 LTS support.
+        if (version_compare(TYPO3_version, '10.0.0', '>=')) {
+            $extensionName = 'Auth0';
+            $controllerName = \Bitmotion\Auth0\Controller\LoginController::class;
+        } else {
+            $extensionName = 'Bitmotion.Auth0';
+            $controllerName = 'Login';
+        }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addService(
-    'auth0',
-    'auth',
-    \Bitmotion\Auth0\Service\AuthenticationService::class,
-    [
-        'title' => 'Auth0 Authentication',
-        'description' => 'Authenticates with Auth0 credentials.',
-        'subtype' => $subtypes,
-        'available' => true,
-        'priority' => $overrulingPriority,
-        'quality' => $overrulingPriority,
-        'os' => '',
-        'exec' => '',
-        'className' => \Bitmotion\Auth0\Service\AuthenticationService::class
-    ]
+        // Configure Auth0 plugin
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+            $extensionName,
+            'LoginForm',
+            [$controllerName => 'form, login, logout'],
+            [$controllerName => 'form, login, logout']
+        );
+
+        // Load extension configuration
+        $configuration = new \Bitmotion\Auth0\Domain\Transfer\EmAuth0Configuration();
+
+        // Get proper subtypes for authentication service
+        $subtypes = 'authUserFE,getUserFE';
+        if ($configuration->getEnableBackendLogin() === true) {
+            $subtypes .= ',getUserBE,authUserBE';
+        }
+
+        // Get priority for Auth0 Authentication Service
+        $highestPriority = 0;
+        if (is_array($GLOBALS['T3_SERVICES']['auth'])) {
+            foreach ($GLOBALS['T3_SERVICES']['auth'] as $service) {
+                if ($service['priority'] > $highestPriority) {
+                    $highestPriority = $service['priority'];
+                }
+            }
+        }
+        $overrulingPriority = $highestPriority + 10;
+
+        // Register login provider
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addService(
+            $extensionKey,
+            'auth',
+            \Bitmotion\Auth0\Service\AuthenticationService::class,
+            [
+                'title' => 'Auth0 authentication',
+                'description' => 'Authentication with Auth0.',
+                'subtype' => $subtypes,
+                'available' => true,
+                'priority' => $overrulingPriority,
+                'quality' => $overrulingPriority,
+                'os' => '',
+                'exec' => '',
+                'className' => \Bitmotion\Auth0\Service\AuthenticationService::class
+            ]
+        );
+
+        // Register logger
+        $GLOBALS['TYPO3_CONF_VARS']['LOG']['Bitmotion']['Auth0'] = [
+            'writerConfiguration' => [
+                \TYPO3\CMS\Core\Log\LogLevel::DEBUG => [
+                    \TYPO3\CMS\Core\Log\Writer\NullWriter::class => []
+                ],
+            ],
+        ];
+    }, 'auth0'
 );
-
-$signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
-$signalSlotDispatcher->connect(
-    \TYPO3\CMS\Extensionmanager\Utility\InstallUtility::class,
-    'afterExtensionInstall',
-    \Bitmotion\Auth0\Slots\ConfigurationSlot::class,
-    'addCacheHashExcludedParameters'
-);
-
-$GLOBALS['TYPO3_CONF_VARS']['LOG']['Bitmotion']['Auth0'] = [
-    'writerConfiguration' => [
-        \TYPO3\CMS\Core\Log\LogLevel::DEBUG => [
-            \TYPO3\CMS\Core\Log\Writer\NullWriter::class => []
-        ],
-    ],
-];
-
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_userauth.php']['logoff_pre_processing']['auth0'] = \Bitmotion\Auth0\Hooks\SingleSignOutHook::class . '->isResponsible';
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_userauth.php']['logoff_post_processing']['auth0'] = \Bitmotion\Auth0\Hooks\SingleSignOutHook::class . '->performLogout';
--- ext_tables.php
+++ ext_tables.php
@@ -1,13 +1,46 @@
 <?php
-defined('TYPO3_MODE') || die();
+defined('TYPO3_MODE') || die('Access denied.');
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:auth0/Configuration/TsConfig/Page/ContentElementWizard/setup.tsconfig">'
+call_user_func(
+    function ($extensionKey) {
+        // Add content element wizzard to PageTSConfig
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+            '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:auth0/Configuration/TsConfig/Page/ContentElementWizard/setup.tsconfig">'
+        );
+
+        // Register icons
+        $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+        $iconRegistry->registerIcon(
+            'auth0',
+            \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class, array(
+            'source' => 'EXT:auth0/Resources/Public/Icons/auth0.svg'
+        ));
+
+        // Register backend hooks
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['list_type_Info']['auth0_loginform'][$extensionKey]
+            = \Bitmotion\Auth0\Hooks\PageLayoutViewHook::class . '->getSummary';
+        // TODO: Support following hooks for frontend request as well and move to ext_localconf.php file
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_userauth.php']['logoff_pre_processing'][$extensionKey]
+            = \Bitmotion\Auth0\Hooks\SingleSignOutHook::class . '->isResponsible';
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_userauth.php']['logoff_post_processing'][$extensionKey]
+            = \Bitmotion\Auth0\Hooks\SingleSignOutHook::class . '->performLogout';
+
+        // Connect to signal slots
+        $signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
+        $signalSlotDispatcher->connect(
+            \TYPO3\CMS\Extensionmanager\Utility\InstallUtility::class,
+            'afterExtensionInstall',
+            \Bitmotion\Auth0\Slots\ConfigurationSlot::class,
+            'addCacheHashExcludedParameters'
+        );
+
+        // Register backend login provider
+        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['backend']['loginProviders'][\Bitmotion\Auth0\LoginProvider\Auth0Provider::LOGIN_PROVIDER] = [
+            'provider' => \Bitmotion\Auth0\LoginProvider\Auth0Provider::class,
+            'sorting' => 25,
+            'icon-class' => 'fa-sign-in',
+            'label' => 'LLL:EXT:auth0/Resources/Private/Language/locallang.xlf:backendLogin.switch.label'
+        ];
+
+    }, 'auth0'
 );
-
-$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-$iconRegistry->registerIcon(
-    'auth0',
-    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class, array(
-    'source' => 'EXT:auth0/Resources/Public/Icons/auth0.svg'
-));
