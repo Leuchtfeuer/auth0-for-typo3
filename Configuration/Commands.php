c18c12b762e129a84956f7797ec09a078af86fb0
Florian Wessels <f.wessels@bitmotion.de>
2020-03-11 13:26:06 +0100

    [TASK] Add todo for deprecated Commands.php

--- Configuration/Commands.php
+++ Configuration/Commands.php
@@ -1,6 +1,13 @@
 <?php
 declare(strict_types = 1);
 
+/*
+ * TODO: Update this when dropping TYPO3 9 LTS support
+ *
+ * As the console commands configuration has been migrated to Symfony service tags, the console command conguration file
+ * Configuration/Commands.php has been marked as deprecated.
+ */
+
 return [
     'auth0:cleanupusers' => [
         'class' => \Bitmotion\Auth0\Command\CleanUpCommand::class,
