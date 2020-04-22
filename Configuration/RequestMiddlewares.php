e8e042ec41bb894b60b668a23ba32fe5f1f7c03f
Florian Wessels <f.wessels@bitmotion.de>
2020-04-22 10:13:34 +0200

    Revert "[TASK] Remove obsolete middleware"
    This reverts commit 5ae4f1f958dd1acba265e2243d9717a2149aa0a3.

--- Configuration/RequestMiddlewares.php
+++ Configuration/RequestMiddlewares.php
@@ -12,5 +12,14 @@ return [
                 'typo3/cms-frontend/base-redirect-resolver',
             ],
         ],
+        'bitmotion/auth0/callback' => [
+            'target' => \Bitmotion\Auth0\Middleware\CallbackMiddleware::class,
+            'after' => ['
+            typo3/cms-frontend/authentication',
+            ],
+            'before' => [
+                'typo3/cms-redirects/redirecthandler',
+            ],
+        ]
     ],
 ];
