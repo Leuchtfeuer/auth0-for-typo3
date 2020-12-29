0cf61cef4f25055ead5ac97adbbcd03790cd1282
Florian Wessels <flo.wessels@gmail.com>
2020-12-29 09:36:46 +0100

    [WIP] GitHub Actions

--- Build/install.sh
+++ Build/install.sh
@@ -4,11 +4,7 @@ if [[ "${TYPO3_VERSION}" = *"dev"*  ]]; then
     composer config minimum-stability dev
 fi
 
-if [[ "${TYPO3_VERSION}" = "dev-master" ]]; then
-    composer req typo3/testing-framework:^5.0 friendsofphp/php-cs-fixer:"${PHP_CS_FIXER_VERSION}" typo3/cms-core:"${TYPO3_VERSION}" typo3/cms-backend:"${TYPO3_VERSION}" typo3/cms-extbase:"${TYPO3_VERSION}" typo3/cms-extensionmanager:"${TYPO3_VERSION}" typo3/cms-fluid:"${TYPO3_VERSION}" typo3/cms-frontend:"${TYPO3_VERSION}"
-else
-    composer req friendsofphp/php-cs-fixer:"${PHP_CS_FIXER_VERSION}" typo3/cms-core:"${TYPO3_VERSION}" typo3/cms-backend:"${TYPO3_VERSION}" typo3/cms-extbase:"${TYPO3_VERSION}" typo3/cms-extensionmanager:"${TYPO3_VERSION}" typo3/cms-fluid:"${TYPO3_VERSION}" typo3/cms-frontend:"${TYPO3_VERSION}"
-fi
+composer req typo3/cms-core:"${TYPO3_VERSION}" typo3/cms-backend:"${TYPO3_VERSION}" typo3/cms-extbase:"${TYPO3_VERSION}" typo3/cms-extensionmanager:"${TYPO3_VERSION}" typo3/cms-fluid:"${TYPO3_VERSION}" typo3/cms-frontend:"${TYPO3_VERSION}"
 
 
 # --------------------------------------------------------------------------- #
