9ec854419be2722f770b1faf1f9e6ed758719320
Florian Wessels <f.wessels@bitmotion.de>
2020-12-29 16:00:30 +0100

    [TASK] Move CI and integration tests to GitHub actions

--- Build/install.sh
+++ Build/install.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-if [[ "${TYPO3_VERSION}" = *"dev"*  ]]; then
-    composer config minimum-stability dev
+if [[ "${TYPO3_VERSION}" == *"dev"* ]]; then
+  composer config minimum-stability dev
 fi
 
-if [[ "${TYPO3_VERSION}" = "dev-master" ]]; then
-    composer req typo3/testing-framework:^5.0 friendsofphp/php-cs-fixer:"${PHP_CS_FIXER_VERSION}" typo3/cms-core:"${TYPO3_VERSION}" typo3/cms-backend:"${TYPO3_VERSION}" typo3/cms-extbase:"${TYPO3_VERSION}" typo3/cms-extensionmanager:"${TYPO3_VERSION}" typo3/cms-fluid:"${TYPO3_VERSION}" typo3/cms-frontend:"${TYPO3_VERSION}"
+composer req typo3/cms-core:"${TYPO3_VERSION}" typo3/cms-backend:"${TYPO3_VERSION}" typo3/cms-extbase:"${TYPO3_VERSION}" typo3/cms-extensionmanager:"${TYPO3_VERSION}" typo3/cms-fluid:"${TYPO3_VERSION}" typo3/cms-frontend:"${TYPO3_VERSION}"
+
+if [[ "${TYPO3_VERSION}" == *"9.5"* ]]; then
+  composer req typo3/testing-framework:^4.15
 else
-    composer req friendsofphp/php-cs-fixer:"${PHP_CS_FIXER_VERSION}" typo3/cms-core:"${TYPO3_VERSION}" typo3/cms-backend:"${TYPO3_VERSION}" typo3/cms-extbase:"${TYPO3_VERSION}" typo3/cms-extensionmanager:"${TYPO3_VERSION}" typo3/cms-fluid:"${TYPO3_VERSION}" typo3/cms-frontend:"${TYPO3_VERSION}"
+  composer req typo3/testing-framework
 fi
-
 
 # --------------------------------------------------------------------------- #
 # Write Fixture file
@@ -24,18 +25,38 @@ echo "<dataset>"
 echo "  <tx_auth0_domain_model_application>"
 echo "    <uid>1</uid>"
 echo "    <pid>0</pid>"
-echo "    <tstamp>1539156784</tstamp>"
+echo "    <tstamp>1609231945</tstamp>"
 echo "    <crdate>1539156784</crdate>"
 echo "    <cruser_id>1</cruser_id>"
 echo "    <deleted>0</deleted>"
 echo "    <hidden>0</hidden>"
-echo "    <title>${APPLICATION}</title>"
-echo "    <id>${CLIENT_ID}</id>"
-echo "    <secret>${CLIENT_SECRET}</secret>"
-echo "    <domain>${CLIENT_DOMAIN}</domain>"
+echo "    <title>Auth0 Test Application</title>"
+echo "    <single_log_out>0</single_log_out>"
+echo "    <id>${AUTH0_CLIENT_ID}</id>"
+echo "    <secret>${AUTH0_CLIENT_SECRET}</secret>"
+echo "    <domain>${AUTH0_DOMAIN}</domain>"
+echo "    <audience>${AUTH0_AUDIENCE}</audience>"
 echo "    <secret_base64_encoded>0</secret_base64_encoded>"
 echo "    <signature_algorithm>RS256</signature_algorithm>"
-echo "    <audience>api/v2/</audience>"
+echo "    <api>1</api>"
+echo "  </tx_auth0_domain_model_application>"
+echo "  <tx_auth0_domain_model_application>"
+echo "    <uid>2</uid>"
+echo "    <pid>0</pid>"
+echo "    <tstamp>1609232145</tstamp>"
+echo "    <crdate>1609232145</crdate>"
+echo "    <cruser_id>1</cruser_id>"
+echo "    <deleted>0</deleted>"
+echo "    <hidden>0</hidden>"
+echo "    <title>Auth0 Test Application (w/o API)</title>"
+echo "    <single_log_out>0</single_log_out>"
+echo "    <id>${AUTH0_CLIENT_ID}</id>"
+echo "    <secret>${AUTH0_CLIENT_SECRET}</secret>"
+echo "    <domain>${AUTH0_DOMAIN}</domain>"
+echo "    <audience/>"
+echo "    <secret_base64_encoded>0</secret_base64_encoded>"
+echo "    <signature_algorithm>RS256</signature_algorithm>"
+echo "    <api>0</api>"
 echo "  </tx_auth0_domain_model_application>"
 echo "</dataset>"
 
