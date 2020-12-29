9a066496ea781f19d6028e1972e15476fd9b5002
Florian Wessels <flo.wessels@gmail.com>
2020-12-29 10:39:17 +0100

    [WIP] GitHub Actions

--- Build/install.sh
+++ Build/install.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
-
-if [[ "${TYPO3_VERSION}" = *"dev"*  ]]; then
-    composer config minimum-stability dev
-fi
-
+composer config minimum-stability dev
 composer req typo3/cms-core:"${TYPO3_VERSION}" typo3/cms-backend:"${TYPO3_VERSION}" typo3/cms-extbase:"${TYPO3_VERSION}" typo3/cms-extensionmanager:"${TYPO3_VERSION}" typo3/cms-fluid:"${TYPO3_VERSION}" typo3/cms-frontend:"${TYPO3_VERSION}"
 
 
@@ -20,18 +16,38 @@ echo "<dataset>"
 echo "  <tx_auth0_domain_model_application>"
 echo "    <uid>1</uid>"
 echo "    <pid>0</pid>"
-echo "    <tstamp>1539156784</tstamp>"
+echo "    <tstamp>1609231945</tstamp>"
 echo "    <crdate>1539156784</crdate>"
 echo "    <cruser_id>1</cruser_id>"
 echo "    <deleted>0</deleted>"
 echo "    <hidden>0</hidden>"
-echo "    <title>${APPLICATION}</title>"
-echo "    <id>${CLIENT_ID}</id>"
-echo "    <secret>${CLIENT_SECRET}</secret>"
-echo "    <domain>${CLIENT_DOMAIN}</domain>"
+echo "    <title>Auth0 Test Applikation</title>"
+echo "    <single_log_out>0</single_log_out>"
+echo "    <id>${AUTH0_CLIENT_ID}</id>"
+echo "    <secret>${AUTH0_CLIENT_SECRET}</secret>"
+echo "    <domain>${AUTH0_DOMAIN}</domain>"
+echo "    <audience>${AUTH0_AUDIENCE}</audience>"
 echo "    <secret_base64_encoded>0</secret_base64_encoded>"
 echo "    <signature_algorithm>RS256</signature_algorithm>"
-echo "    <audience>api/v2/</audience>"
+echo "    <api>1</audience>"
+echo "  </tx_auth0_domain_model_application>"
+echo "  <tx_auth0_domain_model_application>"
+echo "    <uid>2</uid>"
+echo "    <pid>0</pid>"
+echo "    <tstamp>1609232145</tstamp>"
+echo "    <crdate>1609232145</crdate>"
+echo "    <cruser_id>1</cruser_id>"
+echo "    <deleted>0</deleted>"
+echo "    <hidden>0</hidden>"
+echo "    <title>Auth0 Test Applikation (ohne API)</title>"
+echo "    <single_log_out>0</single_log_out>"
+echo "    <id>${AUTH0_CLIENT_ID}</id>"
+echo "    <secret>${AUTH0_CLIENT_SECRET}</secret>"
+echo "    <domain>${AUTH0_DOMAIN}</domain>"
+echo "    <audience></audience>"
+echo "    <secret_base64_encoded>0</secret_base64_encoded>"
+echo "    <signature_algorithm>RS256</signature_algorithm>"
+echo "    <api>0</audience>"
 echo "  </tx_auth0_domain_model_application>"
 echo "</dataset>"
 
--- Build/script.sh
+++ Build/script.sh
@@ -1,33 +1,30 @@
 #!/usr/bin/env bash
 
-if [ -n $TYPO3_DATABASE_NAME ]; then
-	export typo3DatabaseName=$TYPO3_DATABASE_NAME
+if [ -n $MYSQL_DATABASE ]; then
+	export typo3DatabaseName=$MYSQL_DATABASE
 else
 	echo "No environment variable TYPO3_DATABASE_NAME set."
 	exit 1
 fi
 
-if [ -n $TYPO3_DATABASE_HOST ]; then
-	export typo3DatabaseHost=$TYPO3_DATABASE_HOST
+if [ -n $MYSQL_HOST ]; then
+	export typo3DatabaseHost=$MYSQL_HOST
 else
 	echo "No environment variable TYPO3_DATABASE_HOST set."
 	exit 1
 fi
 
-if [ -n $TYPO3_DATABASE_USERNAME ]; then
-	export typo3DatabaseUsername=$TYPO3_DATABASE_USERNAME
+if [ -n $MYSQL_USER ]; then
+	export typo3DatabaseUsername=$MYSQL_USER
 else
 	echo "No environment variable TYPO3_DATABASE_USERNAME set."
 	exit 1
 fi
 
-if [ -n $TYPO3_DATABASE_PASSWORD ]; then
-	export typo3DatabasePassword=$TYPO3_DATABASE_PASSWORD
+if [ -n $MYSQL_PASSWORD ]; then
+	export typo3DatabasePassword=$MYSQL_PASSWORD
 else
 	echo "No environment variable TYPO3_DATABASE_PASSWORD set."
 	exit 1
 fi
 
-
-echo "Running functional API tests"
-.Build/bin/phpunit --colors --configuration Tests/Build/FunctionalTests.xml Tests/Functional --bootstrap .Build/vendor/typo3/testing-framework/Resources/Core/Build/FunctionalTestsBootstrap.php --coverage-clover=coverage.integration.clover
