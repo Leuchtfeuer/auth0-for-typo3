639a626f171fcd38d74d90cb026e4cb885c76965
Max Rösch <github@elbebass.com>
2021-08-30 15:24:07 +0200

    [TASK] Update TER release script

--- Build/release.sh
+++ Build/release.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
 NEW_RELEASE=$1
-LATEST_RELEASE=$(git tag --sort=committerdate --list | tail -1)
 DIR=$(pwd)
 
-
-if [ ! -z "${NEW_RELEASE}" ]; then
+if [ -n "${NEW_RELEASE}" ]; then
   NEW_RELEASE=v${NEW_RELEASE}
   echo "Create release of version ${NEW_RELEASE}"
 else
@@ -13,7 +11,7 @@ else
   exit 1;
 fi
 
-EXISTS=$(git describe --contains ${NEW_RELEASE} 2>&1)
+EXISTS=$(git describe --contains "${NEW_RELEASE}" 2>&1)
 
 if [[ "$EXISTS" == "$NEW_RELEASE" ]]; then
   echo "Release already exists. Exit."
@@ -21,17 +19,20 @@ if [[ "$EXISTS" == "$NEW_RELEASE" ]]; then
 fi
 
 if [ -d "$DIR" ]; then
-  echo "Installing composer dependencies..."
-  cd $DIR/Libraries
-  composer install --no-dev --no-progress --quiet
-  echo "Done."
+
+  if [ -d "$DIR/Libraries" ]; then
+    echo "Installing composer dependencies..."
+    cd "$DIR/Libraries" || exit
+    composer install --no-dev --no-progress --quiet
+    echo "Done."
+  fi
 
   echo "Create git tag"
-  cd $DIR
-  git tag $NEW_RELEASE
+  cd "$DIR" || exit
+  git tag "$NEW_RELEASE"
 
   echo "Archive repository..."
-  zip -r "../auth0_${1}.zip" * -x \*.git\* Build/\* Documentation/\* Tests/\* php-cs-fixer.php codecov.yml LICENSE.txt README.md
+  zip -r "../auth0_${1}.zip" ./* -x \*.git\* Build/\* Documentation/\* Tests/\* php-cs-fixer.php codecov.yml LICENSE.txt README.md
   echo "Done."
 
   echo "Please add and push the git tag: gp --tags"
