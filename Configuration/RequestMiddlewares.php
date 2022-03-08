3fbd7b6cff0890f77dbaf1e777e64e7e0ab2d04f
Max Rösch <github@elbebass.com>
2022-03-08 14:55:56 +0100

    [TASK] Remove Auth0Middleware for the moment
    * Need to discuss the functionality which was planned to implement here.

--- Configuration/RequestMiddlewares.php
+++ Configuration/RequestMiddlewares.php
@@ -3,15 +3,6 @@ declare(strict_types = 1);
 
 return [
     'frontend' => [
-        'bitmotion/auth0' => [
-            'target' => \Bitmotion\Auth0\Middleware\Auth0Middleware::class,
-            'after' => [
-                'typo3/cms-frontend/authentication',
-            ],
-            'before' => [
-                'typo3/cms-frontend/base-redirect-resolver',
-            ],
-        ],
         'bitmotion/auth0/callback' => [
             'target' => \Bitmotion\Auth0\Middleware\CallbackMiddleware::class,
             'after' => [
