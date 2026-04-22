b3b3cb7cb5157a043967a0374fdf548a90be9389
Oliver Heins <o.heins@leuchtfeuer.com>
2026-04-22 14:56:29 +0200

    [TASK] Auto-include TypoScript via ext_localconf.php [TER-453] [TER-432]
    Register TypoScript constants and setup unconditionally so the login mask
    renders correctly without requiring manual template inclusion.

--- ext_localconf.php
+++ ext_localconf.php
@@ -63,3 +63,14 @@ if ($subtypes !== []) {
     );
 }
 
+ExtensionManagementUtility::addTypoScript(
+    'auth0',
+    'constants',
+    "@import 'EXT:auth0/Configuration/TypoScript/constants.typoscript'",
+);
+
+ExtensionManagementUtility::addTypoScript(
+    'auth0',
+    'setup',
+    "@import 'EXT:auth0/Configuration/TypoScript/setup.typoscript'",
+);
