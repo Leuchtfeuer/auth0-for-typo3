7445e72f58f7bb65a53a634c4410d7fa514b18c7
Oliver Heins <o.heins@leuchtfeuer.com>
2026-04-24 11:43:06 +0200

    [BUGFIX] ModeUtility: Fix typo in constant name UNKONWN_MODE -> UNKNOWN_MODE [TER-431] [TER-451]

--- Classes/Utility/ModeUtility.php
+++ Classes/Utility/ModeUtility.php
@@ -19,12 +19,12 @@ use TYPO3\CMS\Core\Http\ApplicationType;
 class ModeUtility
 {
     public const BACKEND_MODE = 'BE';
-    public const UNKONWN_MODE = 'UNKNOWN';
+    public const UNKNOWN_MODE = 'UNKNOWN';
 
     public static function isBackend(?string $mode = null, ?ServerRequestInterface $request = null): bool
     {
         if (!$mode) {
-            $mode = $request instanceof ServerRequestInterface ? self::getModeFromRequest($request) : self::UNKONWN_MODE;
+            $mode = $request instanceof ServerRequestInterface ? self::getModeFromRequest($request) : self::UNKNOWN_MODE;
         }
 
         return $mode === self::BACKEND_MODE;
@@ -34,6 +34,6 @@ class ModeUtility
     {
         return ApplicationType::fromRequest($request)->isBackend()
             ? self::BACKEND_MODE
-            : self::UNKONWN_MODE;
+            : self::UNKNOWN_MODE;
     }
 }
