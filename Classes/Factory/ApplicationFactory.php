33798ffeefe7a3a74b8efb2c45ea59fd0c11e90c
Oliver Heins <o.heins@leuchtfeuer.com>
2026-09-10 12:00:08 +0200

    [TASK] ApplicationFactory: Route management token request through TYPO3 HTTP layer [TER-509]
    The management-token request built its own Guzzle client, so it bypassed
    $GLOBALS['TYPO3_CONF_VARS']['HTTP'] entirely - no proxy, no certificate
    verification settings, no timeouts. Installations behind a proxy had no way to
    make it work.
    
    The request factory needed for the PSR-17 fix is already injected and exposes
    TYPO3's configured client, so the call needs no further dependency. Exception
    behaviour is unchanged: RequestFactory::request() delegates straight to Guzzle
    and still throws GuzzleException.
    
    Backport of the v14 change.

--- Classes/Factory/ApplicationFactory.php
+++ Classes/Factory/ApplicationFactory.php
@@ -14,7 +14,6 @@ namespace Leuchtfeuer\Auth0\Factory;
 use Auth0\SDK\Auth0;
 use Auth0\SDK\Configuration\SdkConfiguration;
 use Auth0\SDK\Exception\ConfigurationException;
-use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 use Leuchtfeuer\Auth0\Domain\Repository\ApplicationRepository;
 use Leuchtfeuer\Auth0\Middleware\CallbackMiddleware;
@@ -63,8 +62,7 @@ class ApplicationFactory
 
         // Management API should be used
         if ($application->hasApi()) {
-            $client = new Client();
-            $response = $client->post($application->getManagementTokenDomain(), [
+            $response = $this->requestFactory->request($application->getManagementTokenDomain(), 'POST', [
                 'form_params' => [
                     'grant_type' => 'client_credentials',
                     'client_id' => $application->getClientId(),
